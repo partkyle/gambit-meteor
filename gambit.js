91422aff490f2cb38edc1fc00c173c97c781f7d3
Kyle Partridge <partkyle@gmail.com>
2012-07-11 01:17:55 -0700

    most of the work from the socket.io project

---
+++ gambit.js
@@ -0,0 +1,135 @@
+Players = new Meteor.Collection('players');
+Rooms = new Meteor.Collection('rooms');
+
+if (Meteor.is_client) {
+
+  var player_id = Players.insert({});
+  Session.set('player_id', player_id);
+  Players.update(player_id, {$set: {name: player_id}});
+
+  var change_room = function(room) {
+    Session.set('room', room);
+    Players.update(Session.get('player_id'), {$set: {room: room}});
+  }
+
+  var change_card = function(card) {
+    Session.set('card', card);
+    Players.update(Session.get('player_id'), {$set: {score: card}});
+  }
+
+  Template.app.lobby_mode = function() {
+    return !Session.get('room');
+  }
+
+  Template.lobby.rooms = function() {
+    return Rooms.find({}, {sort: {name: 1}});
+  }
+
+  Template.lobby.room_name = function() {
+    if (!this.name.length)
+      return this._id
+
+    return this.name;
+  }
+
+  Template.lobby.events = {
+    'click .new-room': function(e) {
+      var room_id = Rooms.insert({name: ''});
+      change_room(room_id);
+    },
+    'click .shelf': function(e) {
+      change_room(this._id);
+    }
+  };
+
+  Template.room.cards = function() {
+    return [ {value: 1}, {value: 2}, {value: 3}, {value: 5}, {value: 8}, {value: 13} ];
+  }
+
+  Template.room.selected = function() {
+    if (this.value == Session.get('card')) {
+      return 'selected';
+    }
+  }
+
+  Template.room.room_name = function() {
+    var current_room = Rooms.findOne({_id: Session.get('room')});
+    return Template.lobby.room_name.apply(current_room);
+  }
+
+  Template.room.player_name = function() {
+    return Players.findOne(Session.get('player_id')).name;
+  }
+
+  Template.room.players = function() {
+    return Players.find({room: Session.get('room')}, {sort: {name: 1}});
+  }
+
+  Template.room.badge_class = function() {
+    if (!this.score) {
+      return '';
+    }
+
+    var badge = 'badge';
+    switch (this.score) {
+      case 2:
+        badge += ' badge-inverse';
+        break;
+      case 3:
+        badge += ' badge-success';
+        break;
+      case 5:
+        badge += ' badge-info';
+        break;
+      case 8:
+        badge += ' badge-warning';
+        break;
+      case 13:
+        badge += ' badge-important';
+        break;
+    }
+
+    return badge;
+  }
+
+  Template.room.score_display = function() {
+    return this.score || '';
+  }
+
+  Template.room.end_round = function() {
+    return Players.find({room: Session.get('room')}).count() == Players.find({room: Session.get('room'), score: {$gt: 0}}).count();
+  }
+
+  Template.room.events = {
+    'click .card': function(e) {
+      if (Session.get('card') == this.value) {
+        change_card(null);
+      } else {
+        change_card(this.value);
+      }
+    },
+    'click .reset': function(e) {
+      Session.set('card', null);
+      Players.update({room: Session.get('room')}, {$set: {score: null}});
+    },
+    'change .room-name': function(e) {
+      var value = String(e.target.value || "");
+      if (value.length) {
+        Rooms.update(Session.get('room'), {$set: {name: value}});
+      }
+    },
+    'change .player-name': function(e) {
+      var value = String(e.target.value || "");
+      if (value.length) {
+        Players.update(Session.get('player_id'), {$set: {name: value}});
+      }
+    }
+  };
+
+}
+
+if (Meteor.is_server) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
